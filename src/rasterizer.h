f5b1e9bc3cc42fc29bb0beef6628091571e72fbb
Luke <43494998+arsity@users.noreply.github.com>
2023-03-10 19:10:21 +0800

    update q1

--- src/rasterizer.h
+++ src/rasterizer.h
@@ -82,7 +82,7 @@ namespace CGL
         // The number of elements in buffer = width * height * sample_rate
         std::vector<Color> sample_buffer;
 
-        bool check_inside(float px, float py, float x0, float y0, float x1, float y1, float x2, float y2);
+        static bool check_inside(float px, float py, float x0, float y0, float x1, float y1, float x2, float y2);
 
      public:
 
